88848e6503e4436ea080a7e1be1e44d8c42ec3fe
RishiDinesh <rishidinesh2002@gmail.com>
2025-11-22 08:54:44 -0500

    optimizer patcher code

---
+++ run_optimized_patcher.py
@@ -0,0 +1,58 @@
+import os
+import json
+import torch
+import pandas as pd
+from pathlib import Path
+from src._types import PatchConfig
+from src.constants import TOP_RFHS_BY_LAYER_HEAD, MODELS_LITERAL
+from src.optimized_patcher import OptimizedActivationPatcher
+
+MODEL_ALIAS: MODELS_LITERAL = "qwen-1p5B"
+
+# set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.mps.is_available():
+    device = torch.device("mps")
+print(f"Using device: {device}")
+
+# load data
+with open("data/traces.jsonl", "r") as f:
+    traces = [json.loads(line) for line in f.readlines()]
+df = pd.DataFrame(traces)
+
+# Transform TOP_RFHS_BY_LAYER_HEAD to map layer -> [rfh_indices] for each model
+layer_to_top_rfhs = {}
+for k, v in TOP_RFHS_BY_LAYER_HEAD.items():
+    d_tmp = {}
+    for cord in v:
+        if cord[0] not in d_tmp:
+            d_tmp[cord[0]] = []
+        d_tmp[cord[0]].append(cord[1])
+    layer_to_top_rfhs[k] = d_tmp
+
+# instantiate patcher
+patcher = OptimizedActivationPatcher(MODEL_ALIAS, device)
+
+# filter dataframe and layer_rfh map for the specific model
+df = df[df["model_name"] == MODEL_ALIAS].reset_index(drop=True)
+layer_to_top_rfhs = layer_to_top_rfhs[MODEL_ALIAS]
+
+# run patching
+savedir = f"data/output/activation_patching/{MODEL_ALIAS}"
+os.makedirs(Path(savedir), exist_ok=True)
+for i, row in df.iterrows():
+    row = row.to_dict()
+    patch_config = PatchConfig(
+        all_rfh = True,
+        layerwise_rfh = True,
+        headwise_rfh = True,
+        all_rfh_savepath = f"{savedir}/all_rfh_heads.jsonl",
+        layerwise_rfh_savepath = f"{savedir}/layer_<LAYER>_rfh_heads.jsonl",
+        headwise_rfh_savepath = f"{savedir}/layer_<LAYER>_rfh_head_<HEAD>.jsonl"
+    )
+    patcher.run(
+        response_withR = row["response_withR"],
+        response_withoutR = row["response_withoutR"],
+        heads_by_layer = layer_to_top_rfhs,
+        config = patch_config
+    )
---
+++ src/_types.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+@dataclass
+class PatchConfig:
+    all_rfh: bool
+    layerwise_rfh: bool
+    headwise_rfh: bool
+    topk_rfh: bool
+    topk_rfh_list: list[tuple[int, int]]|None = None
+    all_rfh_savepath: str|None = None
+    layerwise_rfh_savepath: str|None = None
+    headwise_rfh_savepath: str|None = None
+    topk_rfh_savepath: str|None = None
+
+    def __post_init__(self) -> None:
+        missing = []
+        if self.all_rfh and not self.all_rfh_savepath:
+            missing.append("all_rfh_savepath")
+        if self.layerwise_rfh and not self.layerwise_rfh_savepath:
+            missing.append("layerwise_rfh_savepath")
+        if self.headwise_rfh and not self.headwise_rfh_savepath:
+            missing.append("headwise_rfh_savepath")
+        if self.topk_rfh and (not self.topk_rfh_savepath or not self.topk_rfh_list):
+            missing.append("topk_rfh_savepath")
+        if missing:
+            raise ValueError(f"Savepath(s) required for enabled RFH: {', '.join(missing)}")
+
+
